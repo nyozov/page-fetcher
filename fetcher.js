8655437d0ccb7f1393ab07ac8d9afb65ee984814
Nick Yozov <n_yozov@hotmail.com>
2021-09-22 23:20:50 +0000

    fetcher

---
+++ fetcher.js
@@ -0,0 +1,20 @@
+const request = require('request')
+const fs = require('fs')
+
+const args = process.argv.slice(2)
+
+request(args[0], (error, response, body) => {
+  fs.writeFile(args[1], body, err => {
+    if (err) {
+      console.error(err)
+      return 
+  }
+    console.log(`Downloaded and saved ${body.length} bytes to ${args[1]}`)
+})
+
+  
+  
+});
+
+
+
